420c2771385728a5b342e88dc64c8d1ab621d461
ghzhang <zhangguanhua1996@gmail.com>
2018-03-28 20:22:07 +0800

    add running time and some auxiliary information

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 import os
+import time
 from model.config import Config
 
 if __name__ == "__main__":
-    print("Running Path:\t" + os.getcwd())
+    time_start = time.time()
     config = Config(load=False)
+    logger = config.logger
+    logger.info("Running Path:\t" + os.getcwd())
     if config.use_k_fold:
         dir_model = config.dir_model[:-1]
         for i in range(config.k_fold):
             os.system("python3 main.py --output %s --kth_fold %s" % (dir_model + str(i) + "/", str(i)))
+        os.system("python3 get_result.py >> " + config.dir_evaluate_result)
     else:
         os.system("python3 main.py")
-
-
+    time_end = time.time()
+    logger.info("Running Time: %.2f" % (time_end - time_start))
