8b23f00645632b4169a3e297e9453b4c6e263932
ghzhang <zhangguanhua1996@gmail.com>
2018-04-12 16:20:47 +0800

    add just evaluate

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--output', type=str, help='filename of output weights', default=None)
     parser.add_argument('--kth_fold', type=int, help='time of k fold', default=None)
+    parser.add_argument('--action', type=str, help='evaluate or train&evaluate', choices=['evaluate', 'train'],
+                        default='train')
     args = parser.parse_args()
 
     if args.kth_fold is not None:
@@ -14,9 +16,10 @@ if __name__ == "__main__":
         os.system("python3 build_data.py")
 
     if args.output is not None:
-        os.system("python3 train.py --output " + args.output)
+        if args.action == 'train':
+            os.system("python3 train.py --output " + args.output)
         os.system("python3 evaluate.py --output " + args.output)
     else:
-        os.system("python3 train.py")
+        if args.action == 'train':
+            os.system("python3 train.py")
         os.system("python3 evaluate.py")
-
--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 import os
 import time
 from model.config import Config
+import argparse
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--action', type=str, help='evaluate or train&evaluate', choices=['evaluate', 'train'],
+                        default='train')
+
+    args = parser.parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     time_start = time.time()
     config = Config(load=False)
@@ -11,9 +18,11 @@ if __name__ == "__main__":
     if config.use_k_fold:
         dir_model = config.dir_model[:-1]
         for i in range(config.k_fold):
-            os.system("python3 main.py --output %s --kth_fold %s" % (dir_model + str(i) + "/", str(i)))
+            os.system(
+                "python3 main.py --output %s --kth_fold %s --action %s" % (
+                dir_model + str(i) + "/", str(i), args.action))
         os.system("python3 get_result.py >> " + config.dir_evaluate_result)
     else:
-        os.system("python3 main.py")
+        os.system("python3 main.py --action %s" % (args.action))
     time_end = time.time()
     logger.info("Running Time: %.2f" % (time_end - time_start))
